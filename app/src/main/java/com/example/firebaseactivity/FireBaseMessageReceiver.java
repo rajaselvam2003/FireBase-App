5a77d06cfda27d829ff529593580cb8ce362559a
raja <rajaselvam2003@gmail.com>
2021-10-29 17:45:31 +0800

    FireBaseApp

---
+++ app/src/main/java/com/example/firebaseactivity/FireBaseMessageReceiver.java
@@ -0,0 +1,57 @@
+package com.example.firebaseactivity;
+
+
+import  android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FireBaseMessageReceiver extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.d("TAG", s + " string");
+
+    }
+
+    public static void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TAG", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("TAG", "From: " + remoteMessage.getFrom());
+
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d("TAG", "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+               // scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+               // handleNow();
+            }
+
+        }
+
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d("TAG", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+}
